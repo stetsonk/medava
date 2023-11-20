1ed9e6cde35cc4a88c9030bf45b8f615d283cc4e
Stetson King <stetson.e.king@outlook.com>
2023-11-20 10:29:43 -0500

    line 173 readme

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,30 @@
 package edu.uc.cs3003.medava;
 
 public class Medicine {
+    public Medicine(String medicineName){
+        medicineName = medicineName;
+    }
+
+    public String getMedicineName(){
+        return medicineName;
+    }
+
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature){
+        if(this.minimumTemperature() <= lowTemperature && highTemperature <= this.maximumTemperature()){
+            return true;
+        }
+        return false;
+    }
+
+    public Double minimumTemperature(){
+        return 0.0;
+    }
+
+    public Double maximumTemperature(){
+        return 100.0;
+    }
+    
+
+    private String medicineName;
 
 }
--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -2,5 +2,6 @@ package edu.uc.cs3003.medava;
 
 public class SupplyChain {
     public static void main(String args[]) {
+        //System.out.println("Hello, Stetson! This is the WOPR speaking.");
     }
 }
